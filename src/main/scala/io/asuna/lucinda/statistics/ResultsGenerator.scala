22279556d3f1188bedc7f3fe9c6d37a5fd771e0e
Ian Macalinao <me@ian.pw>
2016-10-22 10:42:46 -0500

    Implement makeScalars

--- src/main/scala/io/asuna/lucinda/statistics/ResultsGenerator.scala
+++ src/main/scala/io/asuna/lucinda/statistics/ResultsGenerator.scala
@@ -8,7 +8,39 @@ import io.asuna.proto.lucinda.LucindaData.ChampionStatistics.{Results, Quotients
 object ResultsGenerator {
 
   def generateResults(quotients: Quotients): Results = {
-    Results()
+    Results(
+      scalars = Some(makeScalars(quotients.scalars.getOrElse(Quotients.Scalars())))
+    )
+  }
+
+  def makeScalars(scalars: Quotients.Scalars): Results.Scalars = {
+    Results.Scalars(
+      wins = makeStat(scalars.wins),
+      goldEarned = makeStat(scalars.goldEarned),
+      kills = makeStat(scalars.kills),
+      deaths = makeStat(scalars.deaths),
+      assists = makeStat(scalars.assists),
+      damageDealt = makeStat(scalars.damageDealt),
+      damageTaken = makeStat(scalars.damageTaken),
+      minionsKilled = makeStat(scalars.minionsKilled),
+      teamJungleMinionsKilled = makeStat(scalars.teamJungleMinionsKilled),
+      enemyJungleMinionsKilled = makeStat(scalars.enemyJungleMinionsKilled),
+      structureDamage = makeStat(scalars.structureDamage),
+      killingSpree = makeStat(scalars.killingSpree),
+      wardsBought = makeStat(scalars.wardsBought),
+      wardsPlaced = makeStat(scalars.wardsPlaced),
+      wardsKilled = makeStat(scalars.wardsKilled),
+      crowdControl = makeStat(scalars.crowdControl),
+      firstBlood = makeStat(scalars.firstBlood),
+      firstBloodAssist = makeStat(scalars.firstBloodAssist),
+      doublekills = makeStat(scalars.doublekills),
+      triplekills = makeStat(scalars.triplekills),
+      quadrakills = makeStat(scalars.quadrakills),
+      pentakills = makeStat(scalars.pentakills),
+      physicalDamage = makeStat(scalars.physicalDamage),
+      magicDamage = makeStat(scalars.magicDamage),
+      trueDamage = makeStat(scalars.trueDamage)
+    )
   }
 
   def makeStat(statsMap: Map[Int, Double]): Map[Int, Results.Statistic] = {
