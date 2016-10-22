d66398679bf09a4902d617429c01e4f43130a4dd
Ian Macalinao <me@ian.pw>
2016-10-22 16:51:38 -0500

    Implement makeStatistics

--- src/main/scala/io/asuna/lucinda/statistics/StatisticsAggregator.scala
+++ src/main/scala/io/asuna/lucinda/statistics/StatisticsAggregator.scala
@@ -1,12 +1,27 @@
 package io.asuna.lucinda.statistics
 
+import io.asuna.proto.enums.Role
 import io.asuna.proto.lucinda.LucindaData._
+import io.asuna.proto.match_sum.MatchSum
+import io.asuna.proto.lucinda.LucindaData.ChampionStatistics._
 import io.asuna.lucinda.database.LucindaDatabase
 
 object StatisticsAggregator {
 
   def aggregate()(implicit db: LucindaDatabase): ChampionStatistics = {
     return ChampionStatistics()
+  }
+
+  def makeStatistics(role: Role, rawSums: Map[Int, MatchSum]): Statistics = {
+    val sums = SumCombiner.combineSums(rawSums)
+    val quotients = QuotientsGenerator.generateQuotients(sums)
+    val results = ResultsGenerator.generateResults(quotients)
+    Statistics(
+      role = role,
+      results = Some(results),
+      quotients = Some(quotients),
+      sums = Some(sums)
+    )
   }
 
 }
