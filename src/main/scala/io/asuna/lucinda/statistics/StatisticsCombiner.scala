9435ff0b73a75e248cdb50a29f082883274517bb
Ian Macalinao <me@ian.pw>
2016-11-17 19:32:28 -0600

    Use the monoid

--- src/main/scala/io/asuna/lucinda/statistics/StatisticsCombiner.scala
+++ src/main/scala/io/asuna/lucinda/statistics/StatisticsCombiner.scala
@@ -1,5 +1,6 @@
 package io.asuna.lucinda.statistics
 
+import scalaz.Monoid
 import scalaz.Scalaz._
 import io.asuna.proto.enums.Role
 import io.asuna.proto.lucinda.LucindaData.ChampionStatistics
@@ -40,46 +41,49 @@ object StatisticsCombiner {
     )
   }
 
+  implicit object ScalarsMonoid extends Monoid[ChampionStatistics.Sums.Scalars] {
+
+    def append(a: ChampionStatistics.Sums.Scalars, b: => ChampionStatistics.Sums.Scalars): ChampionStatistics.Sums.Scalars = {
+      ChampionStatistics.Sums.Scalars(
+        plays = a.plays |+| b.plays,
+        wins = a.wins |+| b.wins,
+        goldEarned = a.goldEarned |+| b.goldEarned,
+        kills = a.kills |+| b.kills,
+        deaths = a.deaths |+| b.kills,
+        assists = a.assists |+| b.assists,
+        damageDealt = a.damageDealt |+| b.damageDealt,
+        damageTaken = a.damageTaken |+| b.damageTaken,
+        minionsKilled = a.minionsKilled |+| b.minionsKilled,
+        teamJungleMinionsKilled = a.teamJungleMinionsKilled |+| b.teamJungleMinionsKilled,
+        enemyJungleMinionsKilled = a.enemyJungleMinionsKilled |+| b.enemyJungleMinionsKilled,
+        structureDamage = a.structureDamage |+| b.structureDamage,
+        killingSpree = a.killingSpree |+| b.killingSpree,
+        wardsBought = a.wardsBought |+| b.wardsBought,
+        wardsPlaced = a.wardsPlaced |+| b.wardsPlaced,
+        wardsKilled = a.wardsKilled |+| b.wardsKilled,
+        crowdControl = a.crowdControl |+| b.crowdControl,
+        firstBlood = a.firstBlood |+| b.firstBlood,
+        firstBloodAssist = a.firstBloodAssist |+| b.firstBloodAssist,
+        doublekills = a.doublekills |+| b.doublekills,
+        triplekills = a.triplekills |+| b.triplekills,
+        quadrakills = a.quadrakills |+| b.quadrakills,
+        pentakills = a.pentakills |+| b.pentakills,
+        physicalDamage = a.physicalDamage |+| b.physicalDamage,
+        magicDamage = a.magicDamage |+| b.magicDamage,
+        trueDamage = a.trueDamage |+| b.trueDamage
+      )
+    }
+
+    def zero = ChampionStatistics.Sums.Scalars()
+  }
+
   def addSums(a: ChampionStatistics.Sums, b: ChampionStatistics.Sums): ChampionStatistics.Sums = {
     // TODO(igm): hard part
     ChampionStatistics.Sums(
-      scalars = Option(addScalars(
-        a.scalars.getOrElse(ChampionStatistics.Sums.Scalars()),
-        b.scalars.getOrElse(ChampionStatistics.Sums.Scalars())
-      ))
+      scalars = a.scalars |+| b.scalars
         // TODO(pradyuman): implement
     )
   }
 
-  def addScalars(a: ChampionStatistics.Sums.Scalars, b: ChampionStatistics.Sums.Scalars): ChampionStatistics.Sums.Scalars = {
-    ChampionStatistics.Sums.Scalars(
-      plays = a.plays |+| b.plays,
-      wins = a.wins |+| b.wins,
-      goldEarned = a.goldEarned |+| b.goldEarned,
-      kills = a.kills |+| b.kills,
-      deaths = a.deaths |+| b.kills,
-      assists = a.assists |+| b.assists,
-      damageDealt = a.damageDealt |+| b.damageDealt,
-      damageTaken = a.damageTaken |+| b.damageTaken,
-      minionsKilled = a.minionsKilled |+| b.minionsKilled,
-      teamJungleMinionsKilled = a.teamJungleMinionsKilled |+| b.teamJungleMinionsKilled,
-      enemyJungleMinionsKilled = a.enemyJungleMinionsKilled |+| b.enemyJungleMinionsKilled,
-      structureDamage = a.structureDamage |+| b.structureDamage,
-      killingSpree = a.killingSpree |+| b.killingSpree,
-      wardsBought = a.wardsBought |+| b.wardsBought,
-      wardsPlaced = a.wardsPlaced |+| b.wardsPlaced,
-      wardsKilled = a.wardsKilled |+| b.wardsKilled,
-      crowdControl = a.crowdControl |+| b.crowdControl,
-      firstBlood = a.firstBlood |+| b.firstBlood,
-      firstBloodAssist = a.firstBloodAssist |+| b.firstBloodAssist,
-      doublekills = a.doublekills |+| b.doublekills,
-      triplekills = a.triplekills |+| b.triplekills,
-      quadrakills = a.quadrakills |+| b.quadrakills,
-      pentakills = a.pentakills |+| b.pentakills,
-      physicalDamage = a.physicalDamage |+| b.physicalDamage,
-      magicDamage = a.magicDamage |+| b.magicDamage,
-      trueDamage = a.trueDamage |+| b.trueDamage
-    )
-  }
 
 }
