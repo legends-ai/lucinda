23f496f38a7f43c318b7e756a7adf170aa2449a5
Ian Macalinao <me@ian.pw>
2016-11-20 21:27:54 -0600

    Move get champion logic to the champion dao

--- src/main/scala/io/asuna/lucinda/LucindaServer.scala
+++ src/main/scala/io/asuna/lucinda/LucindaServer.scala
@@ -2,8 +2,10 @@ package io.asuna.lucinda
 
 import io.asuna.asunasan.Config
 import io.asuna.proto.service_vulgate.{ VulgateGrpc, VulgateRpc }
+import io.asuna.proto.vulgate.VulgateData
 import scala.concurrent.{ExecutionContext, Future}
 
+import scalaz.Scalaz._
 import io.asuna.lucinda.dao.{ChampionDAO, MatchAggregateDAO, ChampionStatisticsDAO}
 import io.asuna.lucinda.database.{Connector, LucindaDatabase}
 import io.asuna.proto.lucinda.LucindaData._
@@ -30,9 +32,9 @@ class LucindaServer(config: Config[LucindaConfig]) extends LucindaGrpc.Lucinda {
   // We'll use lazy vals because the compiler should do the work of figuring the order out.
 
   lazy val redis = RedisClient() // TOOD(igm): use separate redis databases for all types of data we store
-  lazy val championDAO = new ChampionDAO(championStatisticsDAO, matchAggregateDAO)
   lazy val championStatisticsDAO = new ChampionStatisticsDAO(db, redis)
   lazy val matchAggregateDAO = new MatchAggregateDAO(db, redis, championStatisticsDAO)
+  lazy val championDAO = new ChampionDAO(vulgate, championStatisticsDAO, matchAggregateDAO)
 
   override def getStatistics(req: GetStatisticsRequest) = {
     val reply = ChampionStatistics()
@@ -40,22 +42,7 @@ class LucindaServer(config: Config[LucindaConfig]) extends LucindaGrpc.Lucinda {
   }
 
   override def getChampion(req: GetChampionRequest) = {
-    for {
-      statisticsCtx <- vulgate.getStatisticsContext(
-        VulgateRpc.GetStatisticsContextRequest(
-          patches = req.patch,
-          tiers = req.tier
-        )
-      )
-      matchAggregate <- matchAggregateDAO.get(
-        statisticsCtx.champions.toSet, statisticsCtx.patches.toSet, statisticsCtx.lastFivePatches.toSet,
-        req.championId, statisticsCtx.tiers.toSet, req.region, req.role, -1, req.minPlayRate
-      )
-    } yield {
-      Champion(
-        matchAggregate = Some(matchAggregate)
-      )
-    }
+    championDAO.getFull(req.tier, req.patch, req.championId, req.region, req.role, req.minPlayRate)
   }
 
   override def getMatchup(req: GetMatchupRequest) = {
--- src/main/scala/io/asuna/lucinda/dao/ChampionDAO.scala
+++ src/main/scala/io/asuna/lucinda/dao/ChampionDAO.scala
@@ -1,11 +1,71 @@
 package io.asuna.lucinda.dao
 
+import io.asuna.proto.charon.CharonData.Static
+import io.asuna.proto.lucinda.LucindaData.Champion
+import scalaz.Scalaz._
 import io.asuna.proto.enums.{ Region, Role }
 import io.asuna.proto.lucinda.LucindaData.Champion.MatchupOverview
 import io.asuna.proto.lucinda.LucindaData.{ Champion, ChampionStatistics }
+import io.asuna.proto.range.{ PatchRange, TierRange }
+import io.asuna.proto.service_vulgate.VulgateGrpc.Vulgate
+import io.asuna.proto.service_vulgate.VulgateRpc
+import io.asuna.proto.vulgate.VulgateData
 import scala.concurrent.{ ExecutionContext, Future }
 
-class ChampionDAO(statisticsDAO: ChampionStatisticsDAO, matchAggregateDAO: MatchAggregateDAO)(implicit ec: ExecutionContext) {
+class ChampionDAO(
+  vulgate: Vulgate, statisticsDAO: ChampionStatisticsDAO, matchAggregateDAO: MatchAggregateDAO
+)(implicit ec: ExecutionContext) {
+
+  def getFull(
+    tiers: Option[TierRange], patches: Option[PatchRange], champion: Int, region: Region,
+    role: Role, minPlayRate: Double, enemy: Int = -1
+  ): Future[Champion] = {
+    val context = VulgateData.Context().some // TODO(igm): implement
+    for {
+      // Initial vulgate request.
+      factors <- vulgate.getAggregationFactors(
+        VulgateRpc.GetAggregationFactorsRequest(
+          context = context,
+          patches = patches,
+          tiers = tiers
+        )
+      )
+
+      // The match aggregate. Our main honcho.
+      // Contains all data that matters.
+      matchAggregate <- matchAggregateDAO.get(
+        factors.champions.toSet, factors.patches.toSet, factors.lastFivePatches.toSet,
+        champion, factors.tiers.toSet, region,
+        role, enemy, minPlayRate
+      )
+
+      // Matchup stuff. This is very expensive but fortunately it's cached.
+      enemyStatistics <- statisticsDAO.getForPatches(
+        factors.champions.toSet, factors.tiers.toSet, factors.patches.toSet, region, role, enemy)
+      championStatistics <- statisticsDAO.getForPatches(
+        factors.champions.toSet, factors.tiers.toSet, factors.patches.toSet, region, role, enemy, reverse = true)
+
+      // Vulgate champion data
+      champions <- vulgate.getChampions(
+        VulgateRpc.GetChampionsRequest(
+          context = context,
+
+          // List of all champions we care about.
+          // In theory this list will also include the champion requesting the data.
+          champions = enemyStatistics.sums.flatMap(_.scalars).map(_.plays.keys).getOrElse(Seq()).toSeq
+        )
+      )
+    } yield {
+      Champion(
+        metadata = Champion.Metadata(
+          staticInfo = champions.champions.get(champion)
+            // TODO(igm): patch start and end
+        ).some,
+        matchAggregate = matchAggregate.some,
+        matchups = makeMatchups(enemyStatistics, championStatistics)
+      )
+    }
+  }
 
   def get(
     champions: Set[Int], patches: Set[String], lastFivePatches: Set[String],
@@ -21,27 +81,6 @@ class ChampionDAO(statisticsDAO: ChampionStatisticsDAO, matchAggregateDAO: Match
     } yield Champion(
       matchAggregate = Some(matchAggregate)
     )
-  }
-
-  def getWithMatchups(
-    champions: Set[Int], patches: Set[String], lastFivePatches: Set[String],
-    champion: Int, tiers: Set[Int], region: Region,
-    role: Role, enemy: Int = -1, minPlayRate: Double = 0.05
-  ): Future[Champion] = {
-    for {
-      resChampion <- get(
-        champions, patches, lastFivePatches,
-        champion, tiers, region,
-        role, enemy, minPlayRate
-      )
-      enemyStatistics <- statisticsDAO.getForPatches(champions, tiers, patches, region, role, enemy)
-      championStatistics <- statisticsDAO.getForPatches(champions, tiers, patches, region, role, enemy, reverse = true)
-    } yield {
-      Champion(
-        matchAggregate = resChampion.matchAggregate,
-        matchups = makeMatchups(enemyStatistics, championStatistics)
-      )
-    }
   }
 
   private def makeMatchups(enemy: ChampionStatistics, champion: ChampionStatistics): Seq[MatchupOverview] = {
