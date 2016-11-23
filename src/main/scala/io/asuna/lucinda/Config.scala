811b5759b71b5e23f7d10011310a209636ba2919
Ian Macalinao <me@ian.pw>
2016-11-23 00:24:17 -0600

    the snake convention

--- src/main/scala/io/asuna/lucinda/Config.scala
+++ src/main/scala/io/asuna/lucinda/Config.scala
@@ -11,7 +11,7 @@ object LucindaConfig {
   // TODO(igm): figure out how to use SBT=specified version here
   val parser = new ConfigParser[LucindaConfig](AsunaServiceType.Lucinda, "0.1.0", Set(AsunaServiceType.Vulgate)) {
     // Our list of Cassandra hosts.
-    opt[Seq[String]]("cassandraHosts").valueName("<node1>,<node2>...")
+    opt[Seq[String]]("cassandra_hosts").valueName("<node1>,<node2>...")
       .action((x, c) => c.copy(custom = c.custom.copy(cassandraHosts = x)))
       .text("List of Cassandra hosts to connect to.")
   }
