96fd754804f81c2d2be2732d3b6c8ed88306f376
Ian Macalinao <me@ian.pw>
2016-11-23 00:41:44 -0600

    Add redis host to lucinda config

--- src/main/scala/io/asuna/lucinda/Config.scala
+++ src/main/scala/io/asuna/lucinda/Config.scala
@@ -3,17 +3,23 @@ package io.asuna.lucinda
 import io.asuna.asunasan.{ AsunaServiceType, Config, ConfigParser }
 
 case class LucindaConfig(
-  cassandraHosts: Seq[String] = List("localhost")
+  cassandraHosts: Seq[String] = List("localhost"),
+  redisHost: String = "localhost"
 )
 
 object LucindaConfig {
 
   // TODO(igm): figure out how to use SBT=specified version here
   val parser = new ConfigParser[LucindaConfig](AsunaServiceType.Lucinda, "0.1.0", Set(AsunaServiceType.Vulgate)) {
-    // Our list of Cassandra hosts.
+
     opt[Seq[String]]("cassandra_hosts").valueName("<node1>,<node2>...")
       .action((x, c) => c.copy(custom = c.custom.copy(cassandraHosts = x)))
       .text("List of Cassandra hosts to connect to.")
+
+    opt[Seq[String]]("redis_host").valueName("<node1>,<node2>...")
+      .action((x, c) => c.copy(custom = c.custom.copy(redisHost = x)))
+      .text("Redis host to connect to.")
+
   }
 
   def parse(args: Seq[String]): Option[Config[LucindaConfig]] = {
