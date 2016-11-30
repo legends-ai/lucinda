d44dff71dcee245611be444ffe1ce70fb2dc6d7e
Ian Macalinao <me@ian.pw>
2016-11-30 02:46:32 -0600

    Add some apply functions, more abstraction

--- src/main/scala/io/asuna/lucinda/MatchFilterSpace.scala
+++ src/main/scala/io/asuna/lucinda/MatchFilterSpace.scala
@@ -6,18 +6,22 @@ import io.asuna.proto.match_filters.MatchFilters
 // TODO(igm): find a better place for this to live
 
 /**
-  * A space (set) of match filters.
+  * A space (set) of match filters. This is a 7-dimensional projection of
+  * the space of all MatchSums.
   */
 case class MatchFilterSpace(
-  champion: Int,
-  patch: String,
+  champion: Set[Int],
+  patch: Set[String],
   tiers: Set[Int],
-  region: Region,
-  role: Role,
+  region: Set[Region],
+  role: Set[Role],
   queues: Set[QueueType],
-  enemy: Int
+  enemy: Set[Int]
 ) {
 
+  /**
+    * Converts this into something our database can understand.
+    */
   def toFilterSet: Set[MatchFilters] = for {
     tier <- tiers
     queue <- queues
@@ -31,9 +35,64 @@ case class MatchFilterSpace(
     queue = queue
   )
 
+  /**
+    * Inverts enemy and champion. This is useful for computing matchups.
+    */
   def inverse = {
     copy(champion = enemy, enemy = champion)
   }
 
+  def keyify = {
+    // TODO(pradyuman): make a surefire, human readable key for this
+    toString
+  }
+
 }
 
+object MatchFilterSpace {
+
+  /**
+    * The usual case.
+    */
+  def apply(
+    champion: Int,
+    patch: String,
+    tiers: Set[Int],
+    region: Region,
+    role: Role,
+    queues: Set[QueueType],
+    enemy: Int
+  ): MatchFilterSpace = MatchFilterSpace(
+    champions = Set(champion),
+    patches = Set(patch),
+    tiers = tiers,
+    region = Set(region),
+    role = Set(role),
+    queues = queues,
+    enemy = Set(enemy)
+  )
+
+  /**
+    * Creates a map of champion id -> MatchFilterSpace.
+    */
+  def championsMap(
+    champions: Set[Int],
+    patch: String,
+    tiers: Set[Int],
+    region: Region,
+    role: Role,
+    queues: Set[QueueType],
+    enemy: Int
+  ) = for {
+    champion <- champions
+  } yield MatchFilterSpace(
+    champion = champion,
+    patch = patch,
+    tiers = tiers,
+    region = region,
+    role = role,
+    queues = queues,
+    enemy = enemy
+  )
+
+}
