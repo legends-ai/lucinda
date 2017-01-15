8d5f513c560d9ea54d6a79b42d978473f5c59490
Ian Macalinao <me@ian.pw>
2017-01-15 00:48:19 -0600

    remove unused config

--- src/main/scala/asuna/lucinda/Config.scala
+++ src/main/scala/asuna/lucinda/Config.scala
@@ -5,8 +5,6 @@ import buildinfo.BuildInfo
 import asuna.common.{ Config, ConfigParser }
 
 case class LucindaConfig(
-  matchSumsKeyspace: String = "bacchus_dev",
-  cassandraHosts: Seq[String] = List("localhost"),
   redisHost: String = "localhost",
   redisPort: Int = 6379
 ) {
@@ -22,14 +20,6 @@ object LucindaConfigParser extends ConfigParser[LucindaConfig](
   healthPort = 45046,
   initial = LucindaConfig()
 ) {
-
-  opt[Seq[String]]("cassandra_hosts").valueName("<node1>,<node2>...")
-    .action((x, c) => c.copy(service = c.service.copy(cassandraHosts = x)))
-    .text("List of Cassandra hosts to connect to.")
-
-  opt[String]("match_sums_keyspace").valueName("<host>")
-    .action((x, c) => c.copy(service = c.service.copy(matchSumsKeyspace = x)))
-    .text("Match sums keyspace to connect to.")
 
   opt[String]("redis_host").valueName("<host>")
     .action((x, c) => c.copy(service = c.service.copy(redisHost = x)))
