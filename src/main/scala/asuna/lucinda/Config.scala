f239b30341ad40a74f1c8059bdb6af19015c1f93
Ian Macalinao <me@ian.pw>
2017-03-01 21:26:06 -0600

    add config param for cache expiry

--- src/main/scala/asuna/lucinda/Config.scala
+++ src/main/scala/asuna/lucinda/Config.scala
@@ -3,8 +3,12 @@ package asuna.lucinda
 import asuna.common.config.ConfigParser
 import asuna.proto.league.Queue
 import buildinfo.BuildInfo
+import scala.concurrent.duration._
 
-case class LucindaConfig() {
+case class LucindaConfig(
+  statisticsCacheExpiryMs: Duration = 15.minutes,
+  allChampionStatisticsCacheExpiryMs: Duration = 15.minutes
+) {
   val defaultQueues = Set(
     Queue.RANKED_FLEX_SR, // S7 Flex
     Queue.RANKED_SOLO_5x5, // S7 Solo
@@ -19,4 +23,14 @@ object LucindaConfigParser extends ConfigParser[LucindaConfig](
   port = 45045,
   metaPort = 45046,
   initial = LucindaConfig()
-)
+) {
+
+  opt[Int]("statistics_cache_expiry_ms").valueName("<milliseconds>")
+    .action((x, c) => c.copy(service = c.service.copy(statisticsCacheExpiryMs = x.milliseconds)))
+    .text("Milliseconds until the cache is expired for statistics")
+
+  opt[Int]("all_champion_statistics_cache_expiry_ms").valueName("<milliseconds>")
+    .action((x, c) => c.copy(service = c.service.copy(allChampionStatisticsCacheExpiryMs = x.milliseconds)))
+    .text("Milliseconds until the cache is expired for all champion statistics statistics")
+
+}
