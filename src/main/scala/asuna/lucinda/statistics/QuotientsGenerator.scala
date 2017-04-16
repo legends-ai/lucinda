3af891b0ee8b2fd9639a4f3458495ada3067faa2
Pradyuman Vig <pradyumanvig@outlook.com>
2017-04-15 20:42:46 -0500

    use shapeless for QuotientsGenerator

--- src/main/scala/asuna/lucinda/statistics/QuotientsGenerator.scala
+++ src/main/scala/asuna/lucinda/statistics/QuotientsGenerator.scala
@@ -3,111 +3,86 @@ package asuna.lucinda.statistics
 import scala.math.Numeric
 import scala.math.Numeric.Implicits._
 import asuna.proto.league.MatchSum.Statistics.{ Moments => SMoments }
-import asuna.proto.league.lucinda.MatchQuotient.Statistics.{ Moments => QMoments }
-
 import asuna.proto.league.lucinda.AllChampionStatistics.{ Quotients, Sums }
+import asuna.proto.league.lucinda.MatchQuotient.Statistics.{ Moments => QMoments }
+import cats.implicits._
+import shapeless.{ Generic, HList, ::, HNil, Lazy }
 
-object QuotientsGenerator {
+trait QuotientsDeriver[S, Q] {
+  def derive(sums: S): Q
+}
+
+object QuotientsDeriver {
+
+  def apply[S, Q](implicit deriver: QuotientsDeriver[S, Q]) = deriver
+
+  def pure[S, Q](f: S => Q) = new QuotientsDeriver[S, Q] {
+    def derive(sums: S): Q = f(sums)
+  }
+
   import asuna.common.legends.MomentsHelpers._
 
-  def generateQuotients(sums: Sums): Quotients = {
-    Quotients(
-      scalars = sums.scalars match {
-        case Some(sc) => Some(generateScalarsQuotients(sc))
-        case None => Some(Quotients.Scalars())
-      },
-      deltas = sums.deltas match {
-        case Some(de) => Some(generateDeltasQuotients(de))
-        case None => Some(Quotients.Deltas())
+  implicit val momentsDeriver =
+    pure[Map[Int, SMoments], Map[Int, QMoments]](_.mapValues(_.toQuotient))
+
+  implicit val dragonsDeriver =
+    pure[Seq[Sums.Scalars.DragonStat], Seq[Quotients.Scalars.DragonStat]] { sums =>
+      sums map { stat =>
+        Quotients.Scalars.DragonStat(
+          dragon = stat.dragon,
+          // TODO(p): Figure out if there is a way for shapeless to derive this implicitly
+          value = momentsDeriver.derive(stat.value)
+        )
       }
-    )
+    }
+
+  implicit def optionsDeriver[S, Q](implicit deriver: QuotientsDeriver[S, Q]) =
+    pure[Option[S], Option[Q]](_.map(deriver.derive))
+
+  implicit val hnilDeriver = pure[HNil, HNil](_ => HNil)
+
+  implicit def hlistDeriver[SH, ST <: HList, QH, QT <: HList](
+    implicit
+    hDeriver: Lazy[QuotientsDeriver[SH, QH]],
+    tDeriver: Lazy[QuotientsDeriver[ST, QT]]
+  ) = pure[SH :: ST, QH :: QT] { sums =>
+    hDeriver.value.derive(sums.head) :: tDeriver.value.derive(sums.tail)
   }
 
-  def divide(map: Map[Int, SMoments]): Map[Int, QMoments] = {
-    map.mapValues(_.toQuotient)
-  }
+  implicit def genericDeriver[S, Q, RS, RQ](
+    implicit
+    sGen: Generic.Aux[S, RS],
+    qGen: Generic.Aux[Q, RQ],
+    deriver: QuotientsDeriver[RS, RQ]
+  ) = pure[S, Q](sums => qGen.from(deriver.derive(sGen.to(sums))))
 
-  def generateScalarsQuotients(scalars: Sums.Scalars): Quotients.Scalars = {
-    Quotients.Scalars(
-      wins = divide(scalars.wins),
-      goldEarned = divide(scalars.goldEarned),
-      kills = divide(scalars.kills),
-      deaths = divide(scalars.deaths),
-      assists = divide(scalars.assists),
-      damageDealt = divide(scalars.damageDealt),
-      damageTaken = divide(scalars.damageTaken),
-      minionsKilled = divide(scalars.minionsKilled),
-      teamJungleMinionsKilled = divide(scalars.teamJungleMinionsKilled),
-      enemyJungleMinionsKilled = divide(scalars.enemyJungleMinionsKilled),
-      killingSpree = divide(scalars.killingSpree),
-      wardsBought = divide(scalars.wardsBought),
-      wardsPlaced = divide(scalars.wardsPlaced),
-      wardsKilled = divide(scalars.wardsKilled),
-      crowdControl = divide(scalars.crowdControl),
-      firstBlood = divide(scalars.firstBlood),
-      firstBloodAssist = divide(scalars.firstBloodAssist),
-      doublekills = divide(scalars.doublekills),
-      triplekills = divide(scalars.triplekills),
-      quadrakills = divide(scalars.quadrakills),
-      pentakills = divide(scalars.pentakills),
-      physicalDamage = divide(scalars.physicalDamage),
-      magicDamage = divide(scalars.magicDamage),
-      trueDamage = divide(scalars.trueDamage),
-      baronsEncountered = divide(scalars.baronsEncountered),
-      baronsKilled = divide(scalars.baronsKilled),
-      dragonsEncountered = divideDragons(scalars.dragonsEncountered),
-      dragonsKilled = divideDragons(scalars.dragonsKilled)
-    )
-  }
+  implicit object generateDeriver extends QuotientsDeriver[Sums, Quotients] {
 
-  def divideDragons(sum: Seq[Sums.Scalars.DragonStat]): Seq[Quotients.Scalars.DragonStat] = {
-    sum.map { stat =>
-      Quotients.Scalars.DragonStat(
-        dragon = stat.dragon,
-        value = divide(stat.value)
+    def derive(sums: Sums): Quotients = {
+      val scalarsDeriver = QuotientsDeriver[Sums.Scalars, Quotients.Scalars]
+
+      implicit val deltaDeriver = QuotientsDeriver[Sums.Deltas.Delta, Quotients.Deltas.Delta]
+      val deltasDeriver = QuotientsDeriver[Sums.Deltas, Quotients.Deltas]
+
+      Quotients(
+        scalars = sums.scalars
+          .map(scalarsDeriver.derive)
+          .getOrElse(Quotients.Scalars())
+          .some,
+        deltas = sums.deltas
+          .map(deltasDeriver.derive)
+          .getOrElse(Quotients.Deltas())
+          .some
       )
     }
+
   }
 
-  def generateDeltasQuotients(deltas: Sums.Deltas): Quotients.Deltas = {
-    Quotients.Deltas(
-      csDiff = divideDeltasOption(deltas.csDiff),
-      xpDiff = divideDeltasOption(deltas.xpDiff),
-      damageTakenDiff = divideDeltasOption(deltas.damageTakenDiff),
-      xpPerMin = divideDeltasOption(deltas.xpPerMin),
-      goldPerMin = divideDeltasOption(deltas.goldPerMin),
-      towersPerMin = divideDeltasOption(deltas.towersPerMin),
-      wardsPlaced = divideDeltasOption(deltas.wardsPlaced),
-      damageTaken = divideDeltasOption(deltas.damageTaken)
-    )
-  }
+}
 
-  def divideDeltasOption(delta: Option[Sums.Deltas.Delta]): Option[Quotients.Deltas.Delta] = {
-    delta match {
-      case Some(d) => Some(divideDeltas(d))
-      case None => None
-    }
-  }
+object QuotientsGenerator {
 
-  /**
-    * Generates the quotient from the delta sums and duration distributions.
-    */
-  def divideDeltas(delta: Sums.Deltas.Delta): Quotients.Deltas.Delta = {
-    Quotients.Deltas.Delta(
-      zeroToTen = divide(delta.zeroToTen),
-      tenToTwenty = divide(delta.tenToTwenty),
-      twentyToThirty = divide(delta.twentyToThirty),
-      thirtyToEnd = divide(delta.thirtyToEnd)
-    )
-  }
-
-  def divideScalars[S: Numeric, T: Numeric](divisors: Map[Int, S], scalars: Map[Int, T]): Map[Int, Double] = {
-    scalars.transform((k, v) =>
-      divisors.mapValues(_.toDouble).get(k) match {
-        case Some(divisor) if divisor != 0 => v.toDouble / divisor
-        case _ => 0
-      }
-    )
-  }
+  def generateQuotients(sums: Sums): Quotients =
+    QuotientsDeriver[Sums, Quotients].derive(sums)
 
 }
