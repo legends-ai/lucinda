496ac56455fe255a8dfb17520b66b501ab4113d4
Ian Macalinao <me@ian.pw>
2017-04-22 23:29:16 -0500

    add method to diversify builds

--- src/main/scala/asuna/lucinda/dao/StatisticsDAO.scala
+++ src/main/scala/asuna/lucinda/dao/StatisticsDAO.scala
@@ -1,6 +1,7 @@
 package asuna.lucinda.dao
 
 import asuna.lucinda.matches.MinPickRateDecorator
+import asuna.proto.league.lucinda.MatchQuotient.Collections.ItemList
 import asuna.proto.league._
 import asuna.proto.league.lucinda.Statistics
 import asuna.proto.league.lucinda.rpc.Constraints
@@ -51,14 +52,35 @@ object StatisticsDAO {
     }
   }
 
+  /**
+    * An attempt to bring diversity to the suggested build paths.
+    * Only one build -- highest picked -- will be returned per 3 item set.
+    */
+  def diversifyBuilds(in: Seq[ItemList]): Seq[ItemList] = {
+    in
+      .groupBy(_.items.take(3))
+      .values.toSeq
+      .map(
+        _.sortBy(_.subscalars.map(_.playCount).getOrElse(0)).reverse.headOption
+      ).flatten
+  }
+
+  def diversifyBuildsOfStats(in: Statistics): Statistics = {
+    in.update(_.collections.coreBuilds
+                := diversifyBuilds(in.collections.map(_.coreBuilds).getOrElse(Seq())))
+  }
+
 }
 
 class StatisticsDAO(bareDAO: BareStatisticsDAO) {
+  import StatisticsDAO._
 
-  def compute(key: StatisticsDAO.Key): Task[Statistics] = {
-    bareDAO.get(key.baseKey, forceRefresh = key.constraints.forceRefresh).map { stats =>
-      MinPickRateDecorator.decorate(key.constraints.minPickRate, 10, stats)
-    }
+  def compute(key: Key): Task[Statistics] = {
+    bareDAO.get(key.baseKey, forceRefresh = key.constraints.forceRefresh)
+      .map(diversifyBuildsOfStats)
+      .map { stats =>
+        MinPickRateDecorator.decorate(key.constraints.minPickRate, 10, stats)
+      }
   }
 
 }
